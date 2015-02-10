1c097499f0656dc69bc8c7ef9104e05475679c4f
Roger Fischer <roger.fischer@kaywa.com>
2015-02-10 15:21:03 +0100

    Till point 7

--- run_analysis.R
+++ run_analysis.R
@@ -95,9 +95,17 @@ colnames(msdX) <- msdf2as
 # of each variable for each activity and each subject.
 
 ## 5.1. Using sapply to get the mean of every variable, except for activity which will return NA
-data_up <- sapply(msdX, mean)
+data_up <- apply(msdX, 2, function(x) mean(as.numeric(as.character(x)))) 
+# Or data_up <- sapply(msdX, mean)
+
 
 # 6.Please upload the tidy data set created in step 5 of the instructions. Please upload your 
 # data set as a txt file created with write.table() using row.name=FALSE.
-write.table(data_up, file = "data_up.txt", row.name=FALSE, col.names = TRUE)
+write.table(data_up, file = "data_up.txt", row.names = FALSE, col.names = TRUE)
+# => Check why col.names does not work
 
+
+# 7. Read the data back in with read.table
+data_down <- read.table("data_up.txt", header = TRUE)
+head(data_down)
+tail(data_down)
